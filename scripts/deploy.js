cf40d9d0a85d2acd0958c83bec8ed6347987b453
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-04-14 14:47:45 +0100

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+async function main() {
+  // We get the contract to deploy
+  const whitelistContract = await hre.ethers.getContractFactory("Whitelist");
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+
+  await deployedWhitelistContract.deployed();
+
+  console.log("Whitelist deployed to:", deployedWhitelistContract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
